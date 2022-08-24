1d06b2ba6151704e1403f42c1992664be0e34d1b
Hassan Bokhary <hassanbokhary@Hassans-MacBook-Air.local>
2022-08-24 19:55:50 +0500

    Initail project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Laravel-9.19-with-Livewire-2.10-Basic-Data-Binding-
+It is very simple and easy setup with latest Laravel and LiveWire.
---
+++ resources/views/livewire/posts.blade.php
@@ -0,0 +1,8 @@
+<div>
+    {{-- Success is as dangerous as failure. --}}
+    
+    <p><label>Title</label></p>
+    <p><input wire:model='title' type='text' class="form-control" /></p>
+    <p>{{ $title }}</p>
+
+</div>
